932b388604e1dd9cc20d8eb624d3bcd724755ce4
Charles Ueltschey <chaseuelt@gmail.com>
2024-02-06 12:16:29 -0600

    update(start script): udpate the config paths to use new config files

--- start5gcore.sh
+++ start5gcore.sh
@@ -2,7 +2,7 @@ cd ~/containerization
 
 
 #start nrf
-./nrf/build/nrf/build/nrf -c /nrf/etc/config.yaml -o
+./nrf/build/nrf/build/nrf -c ./oai_cn_configs/nrf/config.yaml -o
 
 
 #start asterik ims
@@ -22,23 +22,23 @@ asterisk -fp
 
 
 #start udr
-./udr/build/udr/build/udr -c udr/etc/config.yaml -o
+./udr/build/udr/build/udr -c ./oai_cn_configs/udr/config.yaml -o
 
 
 
 #start udm
-./udm/build/udm/build/udm -c udm/etc/config.yaml -o
+./udm/build/udm/build/udm -c ./oai_cn_configs/udm/config.yaml -o
 
 
 
 
 #start ausf
-./ausf/build/ausf/build/ausf -c ausf/etc/config.yaml -o
+./ausf/build/ausf/build/ausf -c ./oai_cn_configs/ausf/config.yaml -o
 
 
 
 #start amf
-./amf/build/amf/build/amf -c amf/etc/config.yaml -o
+./amf/build/amf/build/amf -c ./oai_cn_configs/amf/config.yaml -o
 
 
 
@@ -47,7 +47,7 @@ asterisk -fp
 
 #start spgwu
 #./spgwu_tiny/build/spgwu_tiny/build/spgwu_tiny -c /spgwu_tiny/etc/config.yaml -o
-./spgwu_tiny/build/spgw_u/build/spgwu -c ./spgwu_tiny/etc/spgw_u.conf -o
+./spgwu_tiny/build/spgw_u/build/spgwu -c ./oai_cn_configs/spgwu/spgw_u.conf -o
 
 
 chmod +x /usr/local/bin/start_trf.sh
