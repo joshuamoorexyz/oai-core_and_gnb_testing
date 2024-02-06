a83266a1433bfe1c0e738c8567bfe688274d46e3
Charles Ueltschey <chaseuelt@gmail.com>
2024-02-06 17:41:54 -0600

    fix(start script): run all processes in the background

--- start5gcore.sh
+++ start5gcore.sh
@@ -1,73 +1,52 @@
 cd ~/containerization
 
-mkdir logs
+mkdir -p logs
 
-#start nrf
+# Start nrf
+(cd logs && touch nrf.log udr.log udm.log ausf.log amf.log smf.log spgwu.log mysql.log)
 
-cd logs
+# Start asterisk ims
+nohup asterisk -fp &
 
-touch nrf.log
-touch udr.log
-touch udm.log
-touch ausf.log
-touch ausf.log
-touch amf.log
-touch spgwu.log
-touch mysql.log
+# Start oai ext dn (you need to fill this in)
 
-cd ..
+# Start mysql
+nohup service mysql start > ./logs/mysql.log &
+sleep 5
 
+# Start nrf
+nohup ./nrf/build/nrf/build/nrf -c ./oai_cn_configs/nrf/config.yaml -o > ./logs/nrf.log &
+sleep 5
 
+# Start udr
+nohup ./udr/build/udr/build/udr -c ./oai_cn_configs/udr/config.yaml -o > ./logs/udr.log &
+sleep 5
 
-#start asterik ims
+# Start udm
+nohup ./udm/build/udm/build/udm -c ./oai_cn_configs/udm/config.yaml -o > ./logs/udm.log &
+sleep 5
 
-asterisk -fp &
+# Start ausf
+nohup ./ausf/build/ausf/build/ausf -c ./oai_cn_configs/ausf/config.yaml -o > ./logs/ausf.log &
+sleep 5
 
+# Start amf
+nohup ./amf/build/amf/build/amf -c ./oai_cn_configs/amf/config.yaml -o > ./logs/amf.log &
+sleep 5
 
-#start oai ext dn
+# Start smf
+nohup ./smf/build/smf/build/smf -c ./oai_cn_configs/smf/config.yaml -o > ./logs/smf.log &
+sleep 5
 
+# Start spgwu
+nohup ./spgwu_tiny/build/spgw_u/build/spgwu -c ./oai_cn_configs/spgwu/spgw_u.conf -o > ./logs/spgwu.log &
+sleep 5
 
-
-
-#start mysql
-
-service start mysql > ./logs/mysql.log & 
-
-
-#start nrf
-./nrf/build/nrf/build/nrf -c ./oai_cn_configs/nrf/config.yaml -o > ./logs/nrf.log &
-
-
-#start udr
-./udr/build/udr/build/udr -c ./oai_cn_configs/udr/config.yaml -o > ./logs/udr.log & 
-
-
-
-#start udm
-./udm/build/udm/build/udm -c ./oai_cn_configs/udm/config.yaml -o ? ./logs/udm.log &
-
-
-
-
-#start ausf
-./ausf/build/ausf/build/ausf -c ./oai_cn_configs/ausf/config.yaml -o > ./logs/ausf.log &
-
-
-
-#start amf
-./amf/build/amf/build/amf -c ./oai_cn_configs/amf/config.yaml -o > ./logs/amf.log & 
-
-
-#This service does not appear to have built properly
-
-#start spgwu
-./spgwu_tiny/build/spgw_u/build/spgwu -c ./oai_cn_configs/spgwu/spgw_u.conf -o > logs/spgwu.log & 
-
-
+# Make start_trf.sh executable
 chmod +x /usr/local/bin/start_trf.sh
 
-/usr/local/bin/start_trf.sh
+# Run start_trf.sh in the background
+nohup /usr/local/bin/start_trf.sh > /dev/null 2>&1 &
 
-
-
-
+# Return to the original directory
+cd -
