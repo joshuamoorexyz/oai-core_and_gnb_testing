2f90e964fdeabec152d797b171b07c46f0eff492
Charles Ueltschey <chaseuelt@gmail.com>
2024-02-06 16:23:28 -0600

    fix(start script): make scripts run in the background and output to logs

--- Dockerfile
+++ Dockerfile
@@ -15,10 +15,10 @@ RUN chmod +x /usr/local/bin/start5gcore.sh
 
 #build core network services
 RUN /usr/local/bin/open5g_bash.sh
-RUN /usr/local/bin/start5gcore.sh
+#RUN /usr/local/bin/start5gcore.sh
 #expose needed ports
 EXPOSE 80/tcp 9090/tcp 38412/sctp 8080/tcp 8805/udp 2152/udp
 EXPOSE 3306 33060
 
 # run the trf script
-RUN /usr/local/bin/start_trf.sh
+#RUN /usr/local/bin/start_trf.sh
--- start5gcore.sh
+++ start5gcore.sh
@@ -1,13 +1,27 @@
 cd ~/containerization
 
+mkdir logs
 
 #start nrf
-./nrf/build/nrf/build/nrf -c ./oai_cn_configs/nrf/config.yaml -o
+
+cd logs
+
+touch nrf.log
+touch udr.log
+touch udm.log
+touch ausf.log
+touch ausf.log
+touch amf.log
+touch spgwu.log
+touch mysql.log
+
+cd ..
+
 
 
 #start asterik ims
 
-asterisk -fp
+asterisk -fp &
 
 
 #start oai ext dn
@@ -17,37 +31,37 @@ asterisk -fp
 
 #start mysql
 
+service start mysql > ./logs/mysql.log & 
 
 
+#start nrf
+./nrf/build/nrf/build/nrf -c ./oai_cn_configs/nrf/config.yaml -o > ./logs/nrf.log &
 
 
 #start udr
-./udr/build/udr/build/udr -c ./oai_cn_configs/udr/config.yaml -o
+./udr/build/udr/build/udr -c ./oai_cn_configs/udr/config.yaml -o > ./logs/udr.log & 
 
 
 
 #start udm
-./udm/build/udm/build/udm -c ./oai_cn_configs/udm/config.yaml -o
+./udm/build/udm/build/udm -c ./oai_cn_configs/udm/config.yaml -o ? ./logs/udm.log &
 
 
 
 
 #start ausf
-./ausf/build/ausf/build/ausf -c ./oai_cn_configs/ausf/config.yaml -o
+./ausf/build/ausf/build/ausf -c ./oai_cn_configs/ausf/config.yaml -o > ./logs/ausf.log &
 
 
 
 #start amf
-./amf/build/amf/build/amf -c ./oai_cn_configs/amf/config.yaml -o
-
-
+./amf/build/amf/build/amf -c ./oai_cn_configs/amf/config.yaml -o > ./logs/amf.log & 
 
 
 #This service does not appear to have built properly
 
 #start spgwu
-#./spgwu_tiny/build/spgwu_tiny/build/spgwu_tiny -c /spgwu_tiny/etc/config.yaml -o
-./spgwu_tiny/build/spgw_u/build/spgwu -c ./oai_cn_configs/spgwu/spgw_u.conf -o
+./spgwu_tiny/build/spgw_u/build/spgwu -c ./oai_cn_configs/spgwu/spgw_u.conf -o > logs/spgwu.log & 
 
 
 chmod +x /usr/local/bin/start_trf.sh
